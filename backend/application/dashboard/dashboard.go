f35aedc9b0183b1edb9f71d330fbe170d28c4a93
Fabian Feitsch <fabian.feitsch@gesi.de>
2025-01-25 16:01:14 +0100

    feat: add still more data to the dashboard BE

--- backend/application/dashboard/dashboard.go
+++ backend/application/dashboard/dashboard.go
@@ -6,13 +6,17 @@ import (
 	"github.com/fafeitsch/private-running-journal/backend/shared"
 	"math"
 	"slices"
+	"strconv"
+	"strings"
 	"time"
 )
 
 type DashboardDto struct {
-	TotalDistance int     `json:"totalDistance"`
-	TopTracks     []Track `json:"topTracks"`
-	TotalRuns     int     `json:"totalRuns"`
+	TotalDistance    int                `json:"totalDistance"`
+	TopTracks        []Track            `json:"topTracks"`
+	TotalRuns        int                `json:"totalRuns"`
+	MedianDistance   int                `json:"medianDistance"`
+	MonthlyAnalytics []MonthlyAnalytics `json:"analytics"`
 }
 
 type Track struct {
@@ -21,6 +25,14 @@ type Track struct {
 	Parents []string `json:"parents"`
 	Count   int      `json:"count"`
 	Length  int      `json:"length"`
+}
+
+type MonthlyAnalytics struct {
+	Month          int `json:"month"`
+	Year           int `json:"year"`
+	TotalDistance  int `json:"totalDistance"`
+	MedianDistance int `json:"medianDistance"`
+	TotalRuns      int `json:"totalRuns"`
 }
 
 type Assembler struct {
@@ -38,6 +50,11 @@ type Options struct {
 	TopTracks int       `json:"topTracks"`
 }
 
+type entry struct {
+	id     string
+	length int
+}
+
 func (a *Assembler) LoadDashboard(options Options) (*DashboardDto, error) {
 	entries, err := a.sortedEntries.FindJournalEntryIdsBetween(options.From, options.To)
 	if err != nil {
@@ -46,14 +63,17 @@ func (a *Assembler) LoadDashboard(options Options) (*DashboardDto, error) {
 	sum := 0
 	trackCache := make(map[string]shared.Track)
 	trackCounter := make(map[string]int)
-	for _, entry := range entries {
-		loaded, err := a.fileService.ReadJournalEntry(entry)
+	entryPerMonth := make(map[string][]entry)
+	currentMonth := options.From.AddDate(0, 0, -options.From.Day()+1)
+	lastOfToMonth := options.To.AddDate(0, 1, -options.To.Day())
+	for currentMonth.Before(lastOfToMonth) || currentMonth.Equal(lastOfToMonth) {
+		entryPerMonth[currentMonth.Format("2006-01")] = make([]entry, 0)
+		currentMonth = currentMonth.AddDate(0, 1, 0)
+	}
+	for _, entryId := range entries {
+		loaded, err := a.fileService.ReadJournalEntry(entryId)
 		if err != nil {
 			return nil, err
-		}
-		if loaded.CustomLength != nil {
-			sum = sum + *loaded.CustomLength
-			continue
 		}
 		track, ok := trackCache[loaded.TrackId]
 		trackCounter[loaded.TrackId] = trackCounter[loaded.TrackId] + 1
@@ -64,18 +84,66 @@ func (a *Assembler) LoadDashboard(options Options) (*DashboardDto, error) {
 			}
 			trackCache[loaded.TrackId] = track
 		}
-		sum = sum + (track.Waypoints.Length() * loaded.Laps)
+		length := track.Waypoints.Length() * loaded.Laps
+		if loaded.CustomLength != nil {
+			length = *loaded.CustomLength
+		}
+		sum = sum + (length)
+		month := loaded.Date.Format("2006-01")
+		entryPerMonth[month] = append(entryPerMonth[month], entry{
+			id:     loaded.TrackId,
+			length: length,
+		})
 	}
 	topTracks := make([]Track, 0, options.TopTracks)
 	for id, track := range trackCache {
 		topTracks = append(topTracks, Track{Id: id, Name: track.Name, Count: trackCounter[id], Parents: track.Parents, Length: track.Waypoints.Length()})
 	}
-	slices.SortFunc(topTracks, func(e Track, e2 Track) int {
-		return e2.Count - e.Count
-	})
+	monthlyAnalytics := createAnalytics(entryPerMonth)
+	slices.SortFunc(topTracks, compareTracks)
 	return &DashboardDto{
-		TotalDistance: sum,
-		TopTracks:     topTracks[:int(math.Min(float64(options.TopTracks), float64(len(topTracks))))],
-		TotalRuns:     len(entries),
+		TotalDistance:    sum,
+		TopTracks:        topTracks[:int(math.Min(float64(options.TopTracks), float64(len(topTracks))))],
+		TotalRuns:        len(entries),
+		MonthlyAnalytics: monthlyAnalytics,
 	}, nil
+}
+
+func createAnalytics(entries map[string][]entry) []MonthlyAnalytics {
+	result := make([]MonthlyAnalytics, 0, len(entries))
+	for key, list := range entries {
+		splitted := strings.Split(key, "-")
+		year, _ := strconv.Atoi(splitted[0])
+		month, _ := strconv.Atoi(splitted[1])
+		slices.SortFunc(list, func(a, b entry) int {
+			return a.length - b.length
+		})
+		median := 0
+		if len(list) > 0 {
+			median = list[len(list)/2].length
+		}
+		sum := 0
+		for _, entry := range list {
+			sum = sum + entry.length
+		}
+		result = append(result, MonthlyAnalytics{Year: year, Month: month, MedianDistance: median, TotalDistance: sum, TotalRuns: len(list)})
+	}
+	slices.SortFunc(result, func(a, b MonthlyAnalytics) int {
+		if a.Year == b.Year {
+			return a.Month - b.Month
+		}
+		return a.Year - b.Year
+	})
+	return result
+}
+
+func compareTracks(track1 Track, track2 Track) int {
+	compare := track2.Count - track1.Count
+	if compare == 0 {
+		compare = strings.Compare(track2.Name, track1.Name)
+	}
+	if compare == 0 {
+		compare = strings.Compare(strings.Join(track2.Parents, ""), strings.Join(track1.Parents, ""))
+	}
+	return compare
 }
