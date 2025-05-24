ec0767b6d7615da4df13bbae18e39d9f49ed2f2d
Fabian Feitsch <fabian.feitsch@gesi.de>
2025-05-24 16:08:49 +0200

    test: add test for cloning tracks

--- test/tests/030_tracks.spec.ts
+++ test/tests/030_tracks.spec.ts
@@ -50,3 +50,48 @@ test('should show usages correctly', async ({page}) => {
   await expect(page.getByLabel('Streckenlänge')).toHaveValue('10,3')
   await expect(page.getByLabel('Speichern')).toBeDisabled()
 })
+
+test('should clone a track, verify it exists, delete it, and verify it was deleted', async ({page}) => {
+  await page.goto('/');
+  await page.getByTestId(globalSelectors.tracksTab).click();
+  await expect(page.getByTestId(globalSelectors.tracksTab)).toContainText("Strecken");
+
+  await page.getByTestId(trackSelectors.toggler).nth(0).click();
+  const trackNodes = page.getByTestId(trackSelectors.trackNode);
+  await expect(trackNodes).toHaveCount(5);
+  await trackNodes.nth(1).click(); // Select "Farmrunde"
+
+  await expect(page.getByLabel('Streckenname')).toHaveValue('Farmrunde');
+  await expect(page.getByLabel('Kommentar')).toHaveValue('bei Regen sehr matschig');
+
+  await page.getByLabel('Klonen').click();
+
+  await expect(page.url()).toContain('/tracks/new');
+
+  await expect(page.getByLabel('Streckenname')).toHaveValue('');
+  await expect(page.getByLabel('Kommentar')).toHaveValue('bei Regen sehr matschig');
+
+  await page.getByLabel('Streckenname').fill('Cloned Farmrunde');
+  await page.getByLabel('Speichern').click();
+  await page.getByTestId(trackSelectors.createTrackButton).click();
+
+  await page.getByTestId(globalSelectors.tracksTab).click();
+  await expect(trackNodes).toHaveCount(3); //because it was closed after saving the clone
+
+  const clonedTrack = page.getByTestId(trackSelectors.trackNode).filter({ hasText: 'Cloned Farmrunde' });
+
+  await expect(clonedTrack).toBeVisible();
+
+  await clonedTrack.click();
+
+  await expect(page.getByLabel('Streckenname')).toHaveValue('Cloned Farmrunde');
+  await expect(page.getByLabel('Kommentar')).toHaveValue('bei Regen sehr matschig');
+
+  await page.getByLabel('Löschen').click();
+  await expect(page.getByTestId(trackSelectors.deleteTrackConfirmation)).toBeVisible();
+  await page.getByLabel('Löschen').nth(1).click();
+
+  const deletedTrack = page.getByTestId(trackSelectors.trackNode).filter({ hasText: 'Cloned Farmrunde' });
+
+  await expect(deletedTrack).not.toBeVisible();
+});
